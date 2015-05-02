abe58c36d28453c978cc5787d470caac85e17342
ThunderDeveloper <zeppelin.hindenburg@mailbox.hu>
2015-05-02 20:12:38 +0200

    Main Form changed
    Added multiline TextBox and Label.

--- MainAppForm.Designer.cs
+++ MainAppForm.Designer.cs
@@ -28,20 +28,46 @@
 		/// </summary>
 		private void InitializeComponent ()
 		{
+			this.tbPizzaKiszallitasOutput = new System.Windows.Forms.TextBox();
+			this.lblPizzaKiszallitasOutput = new System.Windows.Forms.Label();
 			this.SuspendLayout();
+			// 
+			// tbPizzaKiszallitasOutput
+			// 
+			this.tbPizzaKiszallitasOutput.Location = new System.Drawing.Point(12, 247);
+			this.tbPizzaKiszallitasOutput.Multiline = true;
+			this.tbPizzaKiszallitasOutput.Name = "tbPizzaKiszallitasOutput";
+			this.tbPizzaKiszallitasOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.tbPizzaKiszallitasOutput.Size = new System.Drawing.Size(613, 86);
+			this.tbPizzaKiszallitasOutput.TabIndex = 0;
+			// 
+			// lblPizzaKiszallitasOutput
+			// 
+			this.lblPizzaKiszallitasOutput.AutoSize = true;
+			this.lblPizzaKiszallitasOutput.Location = new System.Drawing.Point(12, 231);
+			this.lblPizzaKiszallitasOutput.Name = "lblPizzaKiszallitasOutput";
+			this.lblPizzaKiszallitasOutput.Size = new System.Drawing.Size(129, 13);
+			this.lblPizzaKiszallitasOutput.TabIndex = 1;
+			this.lblPizzaKiszallitasOutput.Text = "PizzakiSzállítás kimenete:";
 			// 
 			// PizzakiszallitasMainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.ClientSize = new System.Drawing.Size(637, 409);
+			this.Controls.Add(this.lblPizzaKiszallitasOutput);
+			this.Controls.Add(this.tbPizzaKiszallitasOutput);
 			this.Name = "PizzakiszallitasMainForm";
 			this.Text = "Pizzakiszállítás";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.TextBox tbPizzaKiszallitasOutput;
+		private System.Windows.Forms.Label lblPizzaKiszallitasOutput;
 	}
 }
 

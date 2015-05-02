e4a2bd7d0d83f01f431f66a2715dbdaceac103a6
ThunderDeveloper <zeppelin.hindenburg@mailbox.hu>
2015-05-02 18:01:52 +0200

    Created XML Serialization classes
    XML Serializer has not been tested due to build errors.

---
+++ Serialization/Cim.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace PTM_Pizzakiszallitas.Serialization
+{
+	class Cim
+	{
+		[XmlAttribute]
+		public string VarosNev { get; set; }
+		public int IrSzam { get; set; }
+		public string UtcaNev { get; set; }
+		public int HazSzam { get; set; }
+	}
+}

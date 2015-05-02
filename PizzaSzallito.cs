33db0d8ea852bbe2574c2125b10fc048648556c0
ThunderDeveloper <zeppelin.hindenburg@mailbox.hu>
2015-05-02 16:12:09 +0200

    Class PizzaSzallito extended
    Added some methods (initialisation).

--- PizzaSzallito.cs
+++ PizzaSzallito.cs
@@ -7,5 +7,28 @@ namespace PTM_Pizzakiszallitas
 {
 	class PizzaSzallito
 	{
+		// Member declarations
+		Iroda iroda = null;
+		Konyha konyha = null;
+		/*
+		Varos varos = null;
+		Futarok futarok = null;
+		 Rendelesek rendelesek = null;
+		*/
+
+		// Public methods
+		public PizzaSzallito ()
+		{
+			Initialise ();
+		}
+
+		// Private methods
+		private void Initialise ()
+		{
+			iroda = new Iroda ();
+			konyha = new Konyha ();
+
+			return;
+		}
 	}
 }

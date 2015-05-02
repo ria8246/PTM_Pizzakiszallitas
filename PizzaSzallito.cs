5ed798fb0d0d15943a8b95a388db347fe59ab161
ThunderDeveloper <zeppelin.hindenburg@mailbox.hu>
2015-05-02 19:23:39 +0200

    Main class PizzaSzallito extended
    MainLoop method works like a draft.

--- PizzaSzallito.cs
+++ PizzaSzallito.cs
@@ -10,23 +10,44 @@ namespace PTM_Pizzakiszallitas
 		// Member declarations
 		Iroda iroda = null;
 		Konyha konyha = null;
-		/*
-		Varos varos = null;
+		Rendelesek rendelesek = null;
 		Futarok futarok = null;
-		 Rendelesek rendelesek = null;
-		*/
-
+		Varos varos = null;
+		
 		// Public methods
 		public PizzaSzallito ()
 		{
 			Initialise ();
 		}
 
+		public void MainLoop ()
+		{
+			Pizza ElkeszitettPizza = null;
+			Futar KovetkezoSzabadFutar = null;
+			Utvonalterv KiszallitasiUtvonal = null;
+
+			KiszallitasiUtvonal = iroda.UtvonalTervezes (rendelesek);
+			/* ElkeszitettPizza = */ iroda.PizzatKeszittet (rendelesek, konyha); // Void return!!!
+			KovetkezoSzabadFutar = futarok.KovetkezoFutar (); // Not implemented!
+			if (KovetkezoSzabadFutar != null)
+			{
+				konyha.PizzatAtad (ElkeszitettPizza, KovetkezoSzabadFutar); // Not implemented!!!
+				iroda.UtvonaltervAtadasa (KiszallitasiUtvonal, KovetkezoSzabadFutar); // Not implemented!!!
+				iroda.FutartIndit (KovetkezoSzabadFutar, varos); // Planned.
+			}
+
+			return;
+		}
+
 		// Private methods
 		private void Initialise ()
 		{
+			// TODO Rendeléseket XML-ből beolvasni...
 			iroda = new Iroda ();
 			konyha = new Konyha ();
+			rendelesek = new Rendelesek ();
+			futarok = new Futarok ();
+			varos = new Varos ();
 
 			return;
 		}

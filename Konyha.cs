74e0caaaa22fe01606bf17aed261521b0c4611b7
Sayangi <monsters@vipmail.hu>
2015-05-02 12:44:53 +0200

    Konyha osztály
    Nagyából kész, kivéve a ??? részt, 1 TODO

---
+++ Konyha.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PTM_Pizzakiszallitas
+{
+    class Konyha
+    {
+        public Konyha() {
+        }
+
+        public Pizza PizzatSut ( PizzaInfo pizzaInfo ) {
+
+            pizza = new Pizza( pizzaInfo.GetAtmero(), pizzaInfo.GetFeltet() );
+            return pizza;
+
+        }
+
+        
+        //public void PizzatAtad ( Pizza pizza, Futar futar ) {
+        //    TODO
+        //}
+        
+    }
+}

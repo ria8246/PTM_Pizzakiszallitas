6925b93ca58a0f852dc56777b29138aa4e82c56c
Sayangi <monsters@vipmail.hu>
2015-05-03 13:52:03 +0200

    Iroda nem müködő osztály

--- Iroda.cs
+++ Iroda.cs
@@ -6,43 +6,55 @@ using System.Text;
 
 namespace PTM_Pizzakiszallitas
 {
+
+    //A cimek és a megsütött pizzak összerendelésére
+    public struct PizzakEsCimek
+    {
+        public Pizza pizza;
+        public Cim cim;
+    }
+    
+
     class Iroda
     {
-        //Azokat a cimeket tároljuk amelyekből lesz az utvonalterv
-        ArrayList UtvonaltervCimek;
+        //Azokat a cim-pizza párokat tároljuk amelyeket beleveszunk az utvonaltervbe.
+        List<PizzakEsCimek> FeldolgozasAlattiRendelesek;
 
         public Iroda()
         {
-            UtvonaltervCimek = new ArrayList();
+            FeldolgozasAlattiRendelesek = new List<PizzakEsCimek>();
         }
 
         //A Rendelesek közül kiszed egyet, a cimet eltárolja, a pizza_info allapjan
-        //pedig utasitja a konyhat a pizza elkészitésére
+        //pedig utasitja a konyhat a pizza elkészitésére, majd a pizzat is eltarolja
         public void PizzatKeszittet( Rendelesek rendelesek, Konyha konyha ) 
         {
-            //TODO kereses ???
-            rendelesek rendeles = rendelesek.GetRendeles();
-            UtvonaltervCimek.Add( rendeles.GetCim() );
+            PizzakEsCimek pec = new PizzakEsCimek();
+            
+            Rendeles rendeles = rendelesek.GetRendeles();
+            pec.cim = rendeles.GetCim();
 
             PizzaInfo pizzaInfo = rendeles.GetRendeltPizza();
-            konyha.PizzatSut( pizzaInfo );
-            
+            pec.pizza = konyha.PizzatSut( pizzaInfo );
+
+            FeldolgozasAlattiRendelesek.Add( pec );
         }
 
         //A tárolt cimeket sorbarendezi, majd végig megy rajta egy ciklussal és
         //átpakolja az Utvonaltervbe, a már rendezett sorrendben
         public Utvonalterv UtvonalTervezes( Rendelesek rendelesek )
         {
-            UtvonaltervCimek.Sort();
+            FeldolgozasAlattiRendelesek.Sort();
 
             Utvonalterv utvonalterv = new Utvonalterv();
 
-            foreach (Object obj in UtvonaltervCimek)
+            foreach (PizzakEsCimek pec in FeldolgozasAlattiRendelesek)
             {
-                utvonalterv.AddCim(obj);
+                //TODO
+                //utvonalterv.AddCim(pec.cim);
             }
 
-            UtvonaltervCimek.Clear();
+            FeldolgozasAlattiRendelesek.Clear();
 
             return utvonalterv;
 
